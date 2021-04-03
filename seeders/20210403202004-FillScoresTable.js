d97b27a6fadb0c1633a24fbbf5327404e3579b8d
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-04-03 23:38:29 +0200

    Seeder fill scores

---
+++ seeders/20210403202004-FillScoresTable.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkInsert('Scores', [
+      {
+		wins: 3,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+		wins: 2,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+		wins: 2,
+        userId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+		wins: 1,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('Scores', null, {});
+  }
+};
